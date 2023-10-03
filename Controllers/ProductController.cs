552b9bd82ec8cda35b5074065638ba5b077e1152
Yrakhwas <yrakhwas@gmail.com>
2023-10-03 16:05:37 +0300

    Add ProductController

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MyProject.Data;
+
+namespace MyProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly FoodShopDbContext ctx;
+        public ProductController()
+        {
+            ctx = new FoodShopDbContext();
+        }
+        public ProductController(FoodShopDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        private void LoadIngridients()
+        {
+            this.ViewBag.Pizzas = new SelectList(ctx.pizza.ToList(), "Id", "Name");
+            this.ViewBag.Sushis = new SelectList(ctx.sushi.ToList(), "Id", "Name");
+            this.ViewBag.Salads = new SelectList(ctx.salads.ToList(), "Id", "Name");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
